1b6042c5aea87154669b689c78c30bc1111a2f1a
Ludwig Granstedt <71264282+ludegra@users.noreply.github.com>
2022-12-01 14:49:30 +0100

    initial commit

---
+++ sketch/blink-deluxe/blink-deluxe.ino
@@ -0,0 +1,13 @@
+#include <openGLCD.h>
+
+void setup()
+{
+    gText text_top = gText(textAreaFULL);
+    text_top.SelectFont(System5x7);
+    text_top.println("Hello World!");
+}
+
+void loop()
+{
+
+}
