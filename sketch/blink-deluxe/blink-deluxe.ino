ddd830f6a3f8844ae0428b2e15281f7df9ef2198
Ludwig Granstedt <71264282+ludegra@users.noreply.github.com>
2023-01-08 13:54:24 +0100

    fixed menu display issues

--- sketch/blink-deluxe/blink-deluxe.ino
+++ sketch/blink-deluxe/blink-deluxe.ino
@@ -1,41 +1,40 @@
-/*
- *  openGLCD Library - Hello World
- * 
- * This sketch prints "Hello World!" to the LCD
- * and shows the time in seconds since Arduino board was reset.
- * 
- *  2013-06-15 bperrybap   - updates for openGLCD
- *  2011-09-14 Bill Perry  - original creation
- *  	bperrybap@opensource.billsworld.billandterrie.com
- */
-
-// include the library header
-// no font headers have to be included
 #include <openGLCD.h>
+#include <blink-deluxe.h>
+
+Menu main_menu;
+MenuItem items[] = {MenuItem("Example 1", NULL), MenuItem("Example 2", NULL), MenuItem("Example 3", NULL)};
 
 void setup()
 {
-  // Initialize the GLCD 
+  Serial.begin(9600); // initialize serial communication at 9600 bits per second:
+  Serial.println("Initializing...");
+
+  Serial.println("Initializing GLCD...");
+  // Initialize the GLCD
   GLCD.Init();
 
- // Select the font for the default text area
+  // Select the font for the default text area
   GLCD.SelectFont(System5x7);
 
-//  GLCD.print(F("hello, world!")); // keep string in flash on AVR boards with IDE 1.x
-//  GLCD.Puts(F("hello, world!")); // Puts() supports F() with any version of IDE
+  Serial.println("Initializing menu...");
+
+
+  main_menu = Menu(items, 3);
+
+  //  GLCD.print(F("hello, world!")); // keep string in flash on AVR boards with IDE 1.x
+  //  GLCD.Puts(F("hello, world!")); // Puts() supports F() with any version of IDE
 
   // print() below uses RAM on AVR boards but works
   // on any version of IDE with any processor
   // note: Same is true for Puts()
-  GLCD.print("hello, world!"); 
 }
 
 void loop()
 {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  GLCD.CursorTo(0, 1);
+  Serial.println("Drawing menu...");
+  GLCD.ClearScreen();
+  main_menu.draw();
+  main_menu.down();
 
-  // print the number of seconds since reset:
-  GLCD.print(millis()/1000);
+  delay(1000);
 }
