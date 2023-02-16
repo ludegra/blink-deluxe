47026e0cc2cd35a8d4021c9adf142babac69ac2a
Ludwig Granstedt <71264282+ludegra@users.noreply.github.com>
2023-02-16 18:23:37 +0100

    stuff

--- lib/loop.cpp
+++ lib/loop.cpp
@@ -7,7 +7,7 @@
 // 3 - down
 // 12 - left
 // 13 - right
-// A5 - select (kinda funky rn)
+// A7 - select
 
 // Pin declarations
 #define UP_PIN 2
@@ -38,9 +38,53 @@ int down_hold_time = 0;
 int left_hold_time = 0;
 int right_hold_time = 0;
 
+const int thresholdValue = 1020;
+
+int analogReadDigital(int analogPin)
+{
+  int analogValue = analogRead(analogPin);
+  if (analogValue < thresholdValue)
+  {
+    return LOW;
+  }
+  else
+  {
+    return HIGH;
+  }
+}
+
 bool check_button_down(int pin, bool *state, bool allow_hold, unsigned long *timestamp = 0, int *delay_index = 0)
 {
   if (digitalRead(pin) == HIGH)
+  {
+    // Check if button is not already pressed
+    if (!*state)
+    {
+      *state = true;
+      *timestamp = millis();
+      *delay_index = 0;
+      return true;
+    }
+    // If it is pressed, check if button is being held down
+    else if (allow_hold && millis() >= *timestamp + hold_delays[*delay_index])
+    {
+      *timestamp = millis();
+      int hold_time_max = sizeof(hold_delays) / sizeof(hold_delays[0]);
+      *delay_index = constrain(*delay_index + 1, 0, hold_time_max - 1);
+      return true;
+    }
+  }
+  else
+  {
+    // If button is not pressed, set state to false
+    *state = false;
+  }
+  return false;
+}
+
+bool check_button_down_analog(int pin, bool *state, bool allow_hold, unsigned long *timestamp = 0, int *delay_index = 0)
+{
+  if (analogReadDigital(pin) == HIGH)
   {
     // Check if button is not already pressed
     if (!*state)
@@ -73,23 +117,28 @@ void Loop::update(Menu *menu)
   // Check buttons
   if (check_button_down(DOWN_PIN, &down_pressed, true, &down_timestamp, &down_hold_time))
   {
+    Serial.println("Down pressed");
     menu->down();
   }
   // For some reason D2 (UP_PIN) reads HIGH for a split second when D3 (DOWN_PIN) is pressed
   if (check_button_down(UP_PIN, &up_pressed, true, &up_timestamp, &up_hold_time) && !down_pressed)
   {
+    Serial.println("Up pressed");
     menu->up();
   }
   if (check_button_down(LEFT_PIN, &left_pressed, true, &left_timestamp, &left_hold_time))
   {
+    Serial.println("Left pressed");
     menu->left();
   }
   if (check_button_down(RIGHT_PIN, &right_pressed, true, &right_timestamp, &right_hold_time))
   {
+    Serial.println("Right pressed");
     menu->right();
   }
-  // FIXME: Select button makes the program go bananas
-  if (check_button_down(SELECT_PIN, &select_pressed, false)) {
+  if (check_button_down_analog(SELECT_PIN, &select_pressed, false))
+  {
+    Serial.println("Select pressed");
     menu->select();
   }
 }
--- sketch/blink-deluxe/blink-deluxe.ino
+++ sketch/blink-deluxe/blink-deluxe.ino
@@ -6,21 +6,26 @@
 // 3 - down
 // 12 - left
 // 13 - right
-// A5 - select (kinda funky rn)
+// A6 - select (kinda funky rn)
 
 // Pin declarations
 #define UP_PIN 2
 #define DOWN_PIN 3
 #define LEFT_PIN 12
 #define RIGHT_PIN 13
-#define SELECT_PIN A7
+#define SELECT_PIN A6
 #define LED_PIN A5
 
 Menu main_menu;
 
+MenuVariable *on_time = new MenuVariable("On time", 100, 0, 100000, 1);
+MenuVariable *off_time = new MenuVariable("Off time", 100, 0, 100000, 1);
+Runner *runner = new Runner("Run", on_time, off_time);
+
 MenuItem* main_menu_items[] = {
-  new MenuVariable("On time", 100, 0, 100000, 1),
-  new MenuVariable("Off time", 100, 0, 100000, 1)
+  on_time,
+  off_time,
+  runner
 };
 
 void setup()
@@ -49,12 +54,39 @@ void setup()
 
   main_menu = Menu(main_menu_items, sizeof(main_menu_items) / sizeof(main_menu_items[0]));
 
+  // GLCD.println("Hello, world!");
+
   //  GLCD.print(F("hello, world!")); // keep string in flash on AVR boards with IDE 1.x
   //  GLCD.Puts(F("hello, world!")); // Puts() supports F() with any version of IDE
 
   // print() below uses RAM on AVR boards but works
   // on any version of IDE with any processor
   // note: Same is true for Puts()
+}
+
+void test_pins()
+{
+  // Serial.println("Testing pins...");
+  if (digitalRead(UP_PIN) == HIGH)
+  {
+    Serial.println("Up pressed");
+  }
+  if (digitalRead(DOWN_PIN) == HIGH)
+  {
+    Serial.println("Down pressed");
+  }
+  if (digitalRead(LEFT_PIN) == HIGH)
+  {
+    Serial.println("Left pressed");
+  }
+  if (digitalRead(RIGHT_PIN) == HIGH)
+  {
+    Serial.println("Right pressed");
+  }
+  if (analogReadDigital(SELECT_PIN) == HIGH)
+  {
+    Serial.println("Select pressed");
+  }
 }
 
 void loop()
@@ -65,5 +97,6 @@ void loop()
   // Serial.println("Drawing...");
   Loop::draw(&main_menu);
 
+  // test_pins();
   delay(20);
 }
